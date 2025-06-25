5fb6e42c555a5df15b318dcc59798d4838952f17
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-06-25 15:57:16 -0300

    Add files via upload

--- tcs-jsf-treinamento/src/main/java/br/com/tcs/treinamento/bean/ConsultaFuncionarioBean.java
+++ tcs-jsf-treinamento/src/main/java/br/com/tcs/treinamento/bean/ConsultaFuncionarioBean.java
@@ -1,6 +1,7 @@
 package br.com.tcs.treinamento.bean;
 
 import br.com.tcs.treinamento.entity.Funcionario;
+import org.primefaces.PrimeFaces;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -13,7 +14,48 @@ public class ConsultaFuncionarioBean {
 
     private List<Funcionario> funcionarios = new ArrayList<Funcionario>();
 
+    private String errorMessage;
+
+    public void alterarFuncionario(Funcionario funcionario) {
+        PrimeFaces.current().executeScript("PF('modifyDialog'.show());");
+    }
+
+    public void validarCampos(Funcionario funcionario) {
+        List<String> erros = new ArrayList<>();
+
+        if(funcionario.getTipoDocumento() == null || funcionario.getTipoDocumento().trim().isEmpty()) {
+            erros.add("Tipo de documento não informado.");
+        } else {
+            if(funcionario.getTipoDocumento().equals("CPF")) {
+                if(funcionario.getNumeroCPF() == null || funcionario.getNumeroCPF().trim().isEmpty()
+                        || funcionario.getNumeroCPF().length() < 11) {
+                    erros.add("CPF não informado ou incompleto (deve conter 11 dígitos).");
+                }
+            } else if(funcionario.getTipoDocumento().equals("CNPJ")) {
+                if(funcionario.getNumeroCNPJ() == null || funcionario.getNumeroCNPJ().trim().isEmpty()
+                        || funcionario.getNumeroCNPJ().length() < 14) {
+                    erros.add("CNPJ não informado ou incompleto (deve conter 14 dígitos).");
+                }
+            }
+        }
+
+        if(!erros.isEmpty()) {
+            errorMessage = String.join("\n" + erros);
+            PrimeFaces.current().executeScript("PF('errorDialog'.show());");
+        } else {
+            PrimeFaces.current().executeScript("PF('successDialog'.show());");
+        }
+    }
+
     public List<Funcionario> getFuncionarios() {
         return funcionarios;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
